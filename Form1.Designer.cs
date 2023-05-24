c0429932e9f7e7b74f7c85553df5a6b19a85dd00
HP <HP@DESKTOP-14DI12H>
2023-05-25 03:29:51 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Login_Form
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.textBox4);
+			this.groupBox1.Controls.Add(this.textBox3);
+			this.groupBox1.Controls.Add(this.textBox2);
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Location = new System.Drawing.Point(395, 101);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(310, 348);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Well Come !";
+			this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(49, 201);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(219, 32);
+			this.textBox4.TabIndex = 3;
+			this.textBox4.Text = "Password";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(49, 239);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(219, 29);
+			this.textBox3.TabIndex = 3;
+			this.textBox3.Text = "Confirm password";
+			// 
+			// textBox2
+			// 
+			this.textBox2.AutoCompleteCustomSource.AddRange(new string[] {
+            "email..."});
+			this.textBox2.Location = new System.Drawing.Point(48, 158);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(219, 29);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.Tag = "hi";
+			this.textBox2.Text = "Email Address...";
+			// 
+			// button3
+			// 
+			this.button3.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+			this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.button3.BackColor = System.Drawing.Color.Fuchsia;
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.ForeColor = System.Drawing.Color.White;
+			this.button3.Location = new System.Drawing.Point(48, 289);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(220, 31);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "SignUp";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+			this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.button2.BackColor = System.Drawing.Color.Fuchsia;
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.ForeColor = System.Drawing.Color.White;
+			this.button2.Location = new System.Drawing.Point(154, 94);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(114, 31);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "SignUp";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button1
+			// 
+			this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+			this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(48, 94);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(107, 31);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Login";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(80, 33);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(135, 27);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "SignUp Form";
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// Form1
+			// 
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = global::Login_Form.Properties.Resources.walpaper;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(1062, 546);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Name = "Form1";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.Load += new System.EventHandler(this.Form1_Load_1);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button3;
+		public System.Windows.Forms.TextBox textBox2;
+	}
+}
+
